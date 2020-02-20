d9bd0b357ba3a232d52b17dd2964e45e1b055968
Ramiro Guzmán C <ramiroguzmanc@gmail.com>
2020-02-20 09:48:43 -0500

    Aprendiendo sobre variables

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-var name;
+var name = 'Ramiro',
+    lsname = 'Guzmán C.',
+    age = 21;
 
-name = 'Ramiro';
+var upName = name.toUpperCase();
+var lwLsname = lsname.toLowerCase();
+var fullName = `${name} ${lsname}`;
+var str = name.substr(1, 3);
 
-console.log('Bienvenido de vuelta, ' + name);
+console.log('Bienvenido de nuevo, ' + fullName);
+console.log('Sé que tienes ' + age + ' años');
+console.log('Y que estás aprendiendo a programarme');
+console.log('--------------------');
+console.log('La última letra de tu nombre es: ' + name.charAt(name.length - 1));
+
+
+alert('Oye, mira tu consola 🤭');
+
+var precio = 200.3;
+var total = precio * 100 * 3 / 100;
