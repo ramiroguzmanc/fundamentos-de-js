b24480f4f6a66ae88f2375e629f42c91f5f8effe
Ramiro Guzmán C <ramiroguzmanc@gmail.com>
2020-03-02 15:58:26 -0500

    Ejercicio de la lluvia con ciclo Do - while

--- index.js
+++ index.js
@@ -1,44 +1,20 @@
-//Ejercicio de simulación de peso utilizando el ciclo while y el método Random
+//Ejercicio de la lluvia con ciclo do - while
 
 alert("Oye, mira tu consola 🤭")
 
-var Ramiro = {
-    name: 'Ramiro',
-    lastname: 'Guzmán',
-    age: 21,
-    weight: 60.60
+var cont = 0;
+
+const llueve = () => Math.random() <= 0.25;
+
+do {
+    cont++;
+} while (!llueve())
+
+
+if (cont === 1) {
+
+    console.log(`Fui a ver si llovía ${cont} vez`)
+} else {
+    console.log(`Fui a ver si llovía ${cont} veces`)
+
 }
-
-const CAMBIO_DE_PESO = 0.2;
-
-// function aumentarDePeso(persona) {
-//     return persona.weight += 0.2;
-// } 
-
-const aumentarDePeso = (persona) => persona.weight += CAMBIO_DE_PESO;
-const bajarDePeso = (persona) => persona.weight -= CAMBIO_DE_PESO;
-
-var NumeroDeIncrementos = 0;
-var NumeroDeDecrementos = 0;
-const PESO_DESEADO = Ramiro.weight - 3;
-var DiasTranscurridos = 0;
-
-while (Ramiro.weight > PESO_DESEADO) {
-    var random = Math.random();
-
-    if (random <= 0.25) {
-        aumentarDePeso(Ramiro);
-        NumeroDeIncrementos++;
-    } else if (random <= 0.5) {
-        bajarDePeso(Ramiro);
-        NumeroDeDecrementos++;
-    }
-
-    DiasTranscurridos++;
-    // console.log(`Peso actual de Ramiro: ${Ramiro.weight}`);
-}
-
-console.log(`Al final del año, ${Ramiro.name} pesa ${Ramiro.weight.toFixed(2)}kg`)
-console.log(`Tiempo transcurrido: ${DiasTranscurridos} días`)
-console.log(`Número total de incrementos: ${NumeroDeIncrementos}`)
-console.log(`Número total de decrementos: ${NumeroDeDecrementos}`)
