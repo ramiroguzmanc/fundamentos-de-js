3c5703e6c9bb31a9896c077909e659ddec2cdd2a
Ramiro Guzmán C <ramiroguzmanc@gmail.com>
2020-03-05 17:28:18 -0500

    Herencia prototipal

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-// POO
+// Herencia prototipal
+
+function heredaDe(prototipoHijo, prototipoPadre) {
+    var fn = function() {}
+    fn.prototype = prototipoPadre.prototype;
+    prototipoHijo.prototype = new fn;
+    prototipoHijo.prototype.constructor = prototipoHijo;
+}
 
 function Human(name, lastname, age, height) {
 
@@ -10,8 +17,8 @@ function Human(name, lastname, age, height) {
 }
 
 
-var Ramiro = new Human('Ramiro', 'Guzmán', 21, 1.81);
-var Gabriela = new Human('Gabriela', 'Campos', 24, 1.61);
+// var Ramiro = new Human('Ramiro', 'Guzmán', 21, 1.81);
+// var Gabriela = new Human('Gabriela', 'Campos', 24, 1.61);
 
 const ESTATURA_ALTA = 1.80;
 
@@ -26,11 +33,19 @@ Human.prototype.greeting = function() {
         console.log(`Hola, Soy ${this.name} ${this.lastname}. Tengo ${this.age} años y soy bajito porque mido ${this.height}mts`);
 
     }
+}
 
-
+function Desarrollador(name, lastname) {
+    this.name = name;
+    this.lastname = lastname;
 
 }
 
+heredaDe(Desarrollador, Human);
+
+Desarrollador.prototype.greeting = function() {
+    console.log(`Hola, me llamo ${this.name} ${this.lastname} y soy desarrollador/a`);
+}
 
 
 
