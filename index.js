76cb7f815120ff518a65d551c22bc65111e41f9c
Ramiro Guzmán C <ramiroguzmanc@gmail.com>
2020-03-05 17:43:23 -0500

    Herencia prototipal mejorada y más sencilla, utilizando las nuevas características del lenguaje

--- index.js
+++ index.js
@@ -1,52 +1,38 @@
-// Herencia prototipal
-
-function heredaDe(prototipoHijo, prototipoPadre) {
-    var fn = function() {}
-    fn.prototype = prototipoPadre.prototype;
-    prototipoHijo.prototype = new fn;
-    prototipoHijo.prototype.constructor = prototipoHijo;
-}
-
-function Human(name, lastname, age, height) {
-
-    this.name = name;
-    this.lastname = lastname;
-    this.age = age;
-    this.height = height;
-
-}
-
-
-// var Ramiro = new Human('Ramiro', 'Guzmán', 21, 1.81);
-// var Gabriela = new Human('Gabriela', 'Campos', 24, 1.61);
+// Herencia prototipal más sencilla, utilizando las nuevas características del lenguaje
 
 const ESTATURA_ALTA = 1.80;
 
-Human.prototype.greeting = function() {
 
-    if (this.height >= ESTATURA_ALTA) {
-
-        console.log(`Hola, Soy ${this.name} ${this.lastname}. Tengo ${this.age} años y soy alto porque mido ${this.height}mts`);
-
-    } else {
-
-        console.log(`Hola, Soy ${this.name} ${this.lastname}. Tengo ${this.age} años y soy bajito porque mido ${this.height}mts`);
-
+class Persona {
+    constructor(name, lastname, age, height) {
+        this.name = name;
+        this.lastname = lastname;
+        this.age = age;
+        this.height = height;
+    }
+    saludar() {
+        console.log(`Hola, soy ${this.name} ${this.lastname} y soy una persona con ${this.age} años`)
+    }
+    soyAlto() {
+        return this.height >= ESTATURA_ALTA
     }
 }
 
-function Desarrollador(name, lastname) {
-    this.name = name;
-    this.lastname = lastname;
+
+
+class Desarrollador extends Persona {
+
+    constructor(name, lastname, height) {
+        super(name, lastname, height) //Pasa los parámentros a la clase constructor de la clase superior, en este caso Human
+    }
+
+    saludar() {
+        console.log(`Hola, me llamo ${this.name} ${this.lastname} y soy desarrollador/a`);
+
+    }
 
 }
 
-heredaDe(Desarrollador, Human);
 
-Desarrollador.prototype.greeting = function() {
-    console.log(`Hola, me llamo ${this.name} ${this.lastname} y soy desarrollador/a`);
-}
-
-
-
-// Ramiro.greeting();
+var Ramiro = new Persona('Ramiro', 'Guzmán', 21, 1.81);
+var Gabriela = new Desarrollador('Gabriela', 'Campos', 24, 1.61);
