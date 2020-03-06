04b15057023bf4b6e3085e0b8c43d1690dfcaa4b
Ramiro Guzmán C <ramiroguzmanc@gmail.com>
2020-03-06 16:14:27 -0500

    Uso de la API de Star Wars para hacer uso de los callbacks

--- index.js
+++ index.js
@@ -1,105 +1,14 @@
-// Funciones como parámetros
+//Uso de la API de StarWars para hacer uso de los callbacks
 
-const ESTATURA_ALTA = 1.80;
+var user_personaje = prompt('Introduce el número del personaje que deseas que te salude')
 
+const API_URL = 'https://swapi.co/api/'
+const PEOPLE_URL = 'people/:id'
 
-class Persona {
-    constructor(name, lastname, age, height) {
-        this.name = name;
-        this.lastname = lastname;
-        this.age = age;
-        this.height = height;
-    }
-    saludar(fn) {
-        var { name, lastname, age } = this; // Esto es lo mismo que decir name.this o lastname.this o age.this
-        console.log(`Hola, soy ${name} ${lastname} y soy una persona con ${age} años`);
+const LUKE_URL = `${API_URL}${PEOPLE_URL.replace(':id',user_personaje)}`
+const opts = { crossDomain: true }
 
-        if (fn) {
-            fn(name, lastname, false)
-        }
-    }
-    soyAlto() {
-        return this.height >= ESTATURA_ALTA
-    }
-}
+const onPeopleResponse = (persona) => console.log(`Hola yo soy ${persona.name}`)
 
 
-class Desarrollador extends Persona {
-
-    constructor(name, lastname, height) {
-        super(name, lastname, height) //Pasa los parámentros a la clase constructor de la clase superior, en este caso Human
-    }
-
-    saludar(fn) {
-        var { name, lastname, age } = this; // Esto es lo mismo que decir name.this o lastname.this o age.this
-
-        console.log(`Hola, me llamo ${name} ${lastname} y soy desarrollador/a`);
-
-        if (fn) {
-            fn(name, lastname, true)
-        }
-
-    }
-
-}
-
-function responderSaludo(name, lastname, esDev) {
-    console.log(`Buen día, ${name} ${lastname}`);
-
-    if (esDev) {
-        console.log(`Oh! no sabía que eras Dev`);
-    }
-}
-
-class Animal {
-    constructor(name, color) {
-        this.name = name;
-        this.color = color;
-    }
-
-    saludar(fn) {
-        console.log(`${this.name} es un animal de color ${this.color}`);
-        if (fn) {
-            fn(this.name, false);
-        } else {
-            console.log('Ah bueno te me cuidas, crack')
-        }
-    }
-}
-
-
-class Perro extends Animal {
-    constructor(name, color) {
-        super(name, color);
-    }
-
-    saludar(fn) {
-        console.log('Guau!, guau guau!')
-        if (fn) {
-            fn(this.name, true);
-        }
-    }
-
-}
-
-function responderAnimal(name, esPerro) {
-    if (esPerro) {
-        console.log(`Que lindo ${name}, me encantan los perros`)
-    } else {
-        console.log(`Que lindo ${name}, me gustan los animales`)
-    }
-}
-
-
-var Ramiro = new Desarrollador('Ramiro', 'Guzmán', 21, 1.81);
-var Gabriela = new Persona('Gabriela', 'Campos', 24, 1.61);
-var Luz = new Persona('Luz', 'Cabrera', 52, 1.58);
-var Tobi = new Perro('Tobi', 'Blanco');
-var Firu = new Animal('Firu', 'Negro');
-
-Tobi.saludar(responderAnimal)
-Firu.saludar(responderAnimal)
-
-// Ramiro.saludar(responderSaludo); //Si le pones los () a responderSaludo, estás llamando la función, y no queremos llamarla sino enviarla.
-// Gabriela.saludar(responderSaludo);
-// Luz.saludar();
+$.get(LUKE_URL, opts, onPeopleResponse)
