e56c6ab7b4d9a6e164c487ac9b2e21f89e7813a5
Ramiro Guzmán C <ramiroguzmanc@gmail.com>
2020-03-10 19:53:04 -0500

    Obteniendo un pokemon con una promesa

--- index.js
+++ index.js
@@ -4,43 +4,30 @@ const POKEMON_URL = 'pokemon/:id/'
 const opts = { crossDomain: true }
 
 
-function obtenerPokemon(id, callback) {
-    var url = `${API_URL}${POKEMON_URL.replace(':id',id)}`
+function obtenerPokemon(id) {
 
-    $
-        .get(url, opts, callback)
-        .fail(() => {
-            console.log(`Sucedió un error y no se pudo obtener el personaje ${id}`)
-        })
+    return new Promise((resolve, reject) => {
+
+        const url = `${API_URL}${POKEMON_URL.replace(':id',id)}`
+        $
+            .get(url, opts, function(data) {
+                resolve(data)
+            })
+            .fail(() => reject(id))
+    })
 }
+
+function onError(id) {
+    console.log(`Ocurrió un error al intentar obtener el personaje ${id}`)
+}
+
+obtenerPokemon(1)
+    .then(function(pokemon) {
+        console.log(`El pokemon 1 es ${pokemon.name}`)
+
+    })
+    .catch(onError)
 
 obtenerPokemon(1, function(pokemon) {
     console.log(`Un ${pokemon.name} salvaje aparece!`)
-    obtenerPokemon(2, function(pokemon) {
-        console.log(`Un ${pokemon.name} salvaje aparece!`)
-        obtenerPokemon(3, function(pokemon) {
-            console.log(`Un ${pokemon.name} salvaje aparece!`)
-            obtenerPokemon(4, function(pokemon) {
-                console.log(`Un ${pokemon.name} salvaje aparece!`)
-                obtenerPokemon(5, function(pokemon) {
-                    console.log(`Un ${pokemon.name} salvaje aparece!`)
-                    obtenerPokemon(6, function(pokemon) {
-                        console.log(`Un ${pokemon.name} salvaje aparece!`)
-
-                    })
-                })
-            })
-        })
-    })
 })
-
-
-//var generarRandom = () => (Math.random().toFixed(1) * 100) + 1
-
-// for (i = 0; i < 10; i++) {
-
-//     var random = generarRandom()
-//     console.log(random)
-//     obtenerPokemon(random)
-
-// }
